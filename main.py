a70655b9e418ae74608b8ea003a56d6e4c8ec659
Kamran Azim <Kamazim121212@gmail.com>
2024-04-05 02:16:50 +0100

    Successfully downloaded images using httpx get method and saved them in a folder

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import smtplib
+from random import randint 
+from httpx import get
 from get_html import find_images
 
-
 divs = find_images()
-
 def get_images():
     images = [i.attrs["src"] for i in divs]
     return images
 
-print(get_images())
+images = get_images()
+count = 0
+
+for img in images:
+    count += 1
+    img = get(img)
+
+    with open(f"catfolder\cat_images{count}.png", "wb") as file:
+        file.write(img.content)
+
